687e8fea15bb5427cb6cf406c87c2b52c956ed0b
jmsimmons90 <kutsuukun@gmail.com>
2020-10-12 20:23:39 -0700

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+def my_collect(array)
+    if block_given?
+        i = 0   
+
+    second_array = []
+
+        while i < array.length
+            item = yield array[i] 
+            i = i + 1
+            second_array << item
+        end
+        second_array
+    else
+    array.counter
+    end
+end
 
 
